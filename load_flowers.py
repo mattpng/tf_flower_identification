30372ea79a0e541b38518602f502254c08452760
Matt <mpng@wisc.edu>
2020-04-11 21:08:43 -0500

    mid preprocessing

--- load_flowers.py
+++ load_flowers.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 # AUTOTUNE = tf.data.experimental.AUTOTUNE # not working
-
 import IPython.display as display
 # from PIL import Image # not working
 import numpy as np
@@ -8,18 +7,50 @@ import matplotlib.pyplot as plt
 import os
 import ssl
 
-# retrieve images 
+# Step 1: Retrieve images 
 import pathlib
 data_dir = tf.keras.utils.get_file(origin='https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
                                          fname='flower_photos', untar=True)
 data_dir = pathlib.Path(data_dir)
 
-# test
+# Check that load is successful
 image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
-# CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"])
-# print(CLASS_NAMES)
+print("Image count ", image_count)
+
+CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"])
+print(CLASS_NAMES)
 
 # roses = list(data_dir.glob('roses/*'))
 # for image_path in roses[:3]:
-#     display.display(Image.open(str(image_path)))
+#     display.display(Image.open(str(image_path)))  # can't import image
+
+# Step 2: Load using tf.data
+list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
+
+def get_label(file_path):
+  # convert the path to a list of path components
+  parts = tf.strings.split(file_path, os.path.sep)
+  # The second to last is the class-directory
+  return parts[-2] == CLASS_NAMES
+
+def decode_img(img):
+  # convert the compressed string to a 3D uint8 tensor
+  img = tf.image.decode_jpeg(img, channels=3)
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  # resize the image to the desired size.
+  return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+
+def process_path(file_path):
+  label = get_label(file_path)
+  # load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in labeled_ds.take(1):
+  print("Image shape: ", image.numpy().shape)
+  print("Label: ", label.numpy())
